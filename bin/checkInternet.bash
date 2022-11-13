85e12fef078ce7018c76d4a2f0f2c0cb457cfba7
ladoo4 <ladoo4>
2022-11-13 10:46:08 -0800

    minor change to output reporting for checkInternet.bash

--- bin/checkInternet.bash
+++ bin/checkInternet.bash
@@ -7,7 +7,7 @@ if nc -zw1 $test 443 && echo |openssl s_client -connect $test:443 2>&1 |awk '
   handshake && $1 == "Verification" { if ($2=="OK") exit; exit 1 }
   $1 $2 == "SSLhandshake" { handshake = 1 }'
 then
-  echo "connectivity"
+  echo "Connected"
 else
-  echo "NO connectivity"
+  echo "Not connected"
 fi
